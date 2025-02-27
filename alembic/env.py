87484417a9eb4e2b7eb22f852e842d48afe7e698
Temirlan <temirlan.kazhigerey@gmail.com>
2025-02-27 19:02:41 +0500

    fix: minio

--- alembic/env.py
+++ alembic/env.py
@@ -13,6 +13,7 @@ from app.config.settings import settings
 config = context.config
 config.set_main_option('sqlalchemy.url',settings.DB_URL)
 
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
