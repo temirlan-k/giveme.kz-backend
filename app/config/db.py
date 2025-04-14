fb73efd1ca517d95bf009c2d4cebc234ddb2d07a
Temirlan <temirlan.kazhigerey@gmail.com>
2025-04-14 18:44:32 +0500

    fix: db engine

--- app/config/db.py
+++ app/config/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from app.config.settings import settings
 
 
-engine = create_engine(url=settings.POSTGRES_URL)
+engine = create_engine(url=settings.POSTGRES_URL,pool_pre_ping=True,connect_args={"timeout": 30})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
